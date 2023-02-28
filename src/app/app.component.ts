418dd6cb1899c8f6841e828970bbe1051f799949
Nicolas-NL <you@example.com>
2023-02-28 10:58:58 -0300

    28/02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pw3-introducao';
+   logo = "favicon.ico";
    Nome = "Nicolas";
    Sobrenome="Lima";
    Idade = 17;
+   ocultar = true;
 
    NomeCompleto(){
     this.Nome+this.Sobrenome
     return this.Nome+' '+this.Sobrenome
-
-
    }
+    obterAlerta(){
+      alert("Hoje é Terça-feira");
+    }
+    exibirOcultar(){
+         this.ocultar = !this.ocultar;
+
+       }
+
 }
